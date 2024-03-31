e1e932d4f82af52252ea7580cd981a265f18ea82
Ubuntu <ubuntu@ip-172-31-1-89.us-east-2.compute.internal>
2024-03-31 16:00:16 +0000

    new api key + pem file

--- site/codegradr.py
+++ site/codegradr.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 app.static_folder = 'static'
 app.template_folder ='templates'
 
@@ -13,4 +14,4 @@ def feedback():
     return render_template('feedback.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
