ee8cc0e5fea4118dbb09aa48c256bdd6d8227250
Tatikola, Indira <itatikola@gmail.com>
2024-05-17 20:11:48 -0400

    file upload working

--- site/codegradr.py
+++ site/codegradr.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import openai
+import os
 
 app = Flask(__name__)
 
 app.static_folder = 'static'
 app.template_folder ='templates'
+UPLOAD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'uploads'))
 
 @app.route('/')
 def index():
@@ -12,6 +14,7 @@ def index():
 
 @app.route('/uploads', methods=['POST']) 
 def uploads():
+    print('inside uploads')
     # Check if the POST request has the file part
     if 'test_files' not in request.files or 'solution_files' not in request.files:
         return 'No file part'
@@ -21,18 +24,23 @@ def uploads():
 
     # Check if user selected files
     if len(test_files) == 0 or len(solution_files) == 0:
+        print("no files")
         return 'No files selected'
 
     # Save the uploaded files
     for test_file in test_files:
-        # Save or process test_file
-        test_file.save("path/to/save/test_file")
+       if test_file.filename:  # Only save if a filename is provided
+            print(test_file.filename)
+            test_file.save(os.path.join(UPLOAD_FOLDER, test_file.filename))
+            # test_file.save(os.path.join(UPLOAD_FOLDER, test_file.filename))
 
     for solution_file in solution_files:
-        # Save or process solution_file
-        solution_file.save("path/to/save/solution_file")
+        if solution_file.filename:  # Only save if a filename is provided
+            print(solution_file.filename)
+            test_file.save(os.path.join(UPLOAD_FOLDER, solution_file.filename))
 
-    return 'success'
+    return redirect(url_for('feedback'))
+
 
 @app.route('/feedback')
 def feedback():
