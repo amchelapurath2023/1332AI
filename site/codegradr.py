372a8ceb75902672ce452becccd5367e04b5694c
Tatikola, Indira <itatikola@gmail.com>
2024-05-18 15:50:09 -0400

    compiling java files

--- site/codegradr.py
+++ site/codegradr.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for
 import openai
 import os
 import subprocess
+import glob
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.template_folder ='templates'
 UPLOAD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'uploads'))
+JUNIT_JAR = os.path.join(UPLOAD_FOLDER, 'junit-4.13.2.jar')
+HAMCREST_JAR = os.path.join(UPLOAD_FOLDER, 'hamcrest-core-1.3.jar')
+all_files = glob.glob(os.path.join(UPLOAD_FOLDER, '*.java'))
 
 @app.route('/')
 def index(error=''):
@@ -48,21 +52,36 @@ def compile_and_run_java_files(student, test):
     print("compile_and_run_java_files")
     print(student)
     print(test)
+    print(all_files)
     try:
         # Compile all Java files in the directory
         compile_process = subprocess.run(['javac', student], cwd=UPLOAD_FOLDER, capture_output=True, text=True)
 
         if compile_process.returncode != 0:
-            print('fail here')
             return f"Compilation failed:\n{compile_process.stderr}"
+        else:
+            print("student code compiles")
 
         # Run the test files
+        compile_process = subprocess.run(['javac', '-cp', f'.:{JUNIT_JAR}:{HAMCREST_JAR}'] + all_files, cwd=UPLOAD_FOLDER,
+                                        capture_output=True, text=True)
+        
+        if compile_process.returncode != 0:
+            return f"Compilation failed:\n{compile_process.stderr}"
+        else:
+            print("test code compiles")
+
+        '''
+        test = test[:-5]
+        print(test)
         run_process = subprocess.run(['java', test], cwd=UPLOAD_FOLDER, capture_output=True, text=True)
 
         if run_process.returncode != 0:
+            print('execution failed')
+            print(run_process.stderr)
             return f"Execution failed:\n{run_process.stderr}"
-
-        return run_process.stdout
+        
+        return run_process.stdout'''
 
     except Exception as e:
         return str(e)
@@ -82,15 +101,16 @@ def feedback():
     student_filename = ""
     test_filename = ""
     for file in files:
-        if "Test" not in file and "Node" not in file:
-            student_filename = file
-        if "Test.java" in file:
-            test_filename = file
+        if 'jar' not in file:
+            if "Test" not in file and "Node" not in file:
+                student_filename = file
+            if "Test.java" in file:
+                test_filename = file
         if student_filename and test_filename:
             break
 
     output = compile_and_run_java_files(student_filename, test_filename)
-    print(output)
+    # print(output)
 
     path = UPLOAD_FOLDER + "/" + student_filename
     student_implementation = read_file_to_string(path)
